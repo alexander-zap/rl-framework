6213ad60f88e6bf948c9b783ccbd1a4768c454cc
Alexander Zap <alexander.zap@alexanderthamm.com>
2023-02-23 18:05:45 +0100

    added reward_range as must-have attribute to Environments

--- src/rl_framework/environment/environment.py
+++ src/rl_framework/environment/environment.py
@@ -1,38 +1,112 @@
 import gym
 from abc import abstractmethod
+from typing import Tuple
 
 
 class Environment(gym.Env):
     """
     Environment base class, from which every Environment should be inherited.
 
-    This Environment provides the interface in the OpenAI Gym format (actually it is even inherited from gym.Env).
-    This is a standardized environment interface and should be used for every new environment created.
+    It encapsulates an environment with arbitrary behind-the-scenes dynamics.
 
+    This Environment provides the interface in the Gym format (actually it is even inherited from gym.Env).
+    This is a standardized environment interface and should be used for every new environment created.
     For more guidance, on how to create new custom environments, see following description:
     https://stable-baselines3.readthedocs.io/en/master/guide/custom_env.html
     """
 
     @property
     def action_space(self):
+        """
+        The Space object corresponding to valid actions
+        """
         raise NotImplementedError
 
     @property
     def observation_space(self):
+        """
+        The Space object corresponding to valid observations
+        """
+        raise NotImplementedError
+
+    @property
+    def reward_range(self):
+        """
+        A tuple corresponding to the min and max possible rewards
+        """
         raise NotImplementedError
 
     @abstractmethod
     def __init__(self, *args, **kwargs):
+        """ Initialize the environment.
+        """
         raise NotImplementedError
 
     @abstractmethod
-    def step(self, action):
+    def step(self, action) -> Tuple[object, float, bool, dict]:
+        """Run one timestep of the environment's dynamics. When end of
+        episode is reached, you are responsible for calling `reset()`
+        to reset this environment's state.
+
+        Accepts an action and returns a tuple (observation, reward, done, info).
+
+        Args:
+            action (object): an action provided by the agent
+
+        Returns:
+            Tuple consisting of following elements:
+                observation (object): agent's observation of the current environment
+                reward (float) : amount of reward returned after previous action
+                done (bool): whether the episode has ended, in which case further step() calls will return undefined results
+                info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+        """
         raise NotImplementedError
 
     @abstractmethod
-    def reset(self):
+    def reset(self) -> object:
+        """Resets the environment to an initial state and returns an initial observation.
+
+        Returns:
+            observation (object): the initial observation.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def render(self, mode="human"):
+        """Renders the environment.
+
+            The set of supported modes varies per environment. (And some
+            environments do not support rendering at all.) By convention,
+            if mode is:
+
+            - human: render to the current display or terminal and
+              return nothing. Usually for human consumption.
+            - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
+              representing RGB values for an x-by-y pixel image, suitable
+              for turning into a video.
+            - ansi: Return a string (str) or StringIO.StringIO containing a
+              terminal-style text representation. The text can include newlines
+              and ANSI escape sequences (e.g. for colors).
+
+            Note:
+                Make sure that your class's metadata 'render.modes' key includes
+                  the list of supported modes. It's recommended to call super()
+                  in implementations to use the functionality of this method.
+
+            Args:
+                mode (str): the mode to render with
+
+            Example:
+
+            class MyEnv(Env):
+                metadata = {'render.modes': ['human', 'rgb_array']}
+
+                def render(self, mode='human'):
+                    if mode == 'rgb_array':
+                        return np.array(...) # return RGB frame suitable for video
+                    elif mode == 'human':
+                        ... # pop up a window and render
+                    else:
+                        super(MyEnv, self).render(mode=mode) # just raise an exception
+            """
         raise NotImplementedError
--- src/rl_framework/environment/gym_environment.py
+++ src/rl_framework/environment/gym_environment.py
@@ -4,7 +4,7 @@ from rl_framework.environment import Environment
 
 class GymEnvironmentWrapper(Environment):
     """
-    This is a wrapper for Gym environments.
+    This is a wrapper Environment class for Gym environments.
     """
 
     @property
@@ -15,6 +15,10 @@ class GymEnvironmentWrapper(Environment):
     def observation_space(self):
         return self._observation_space
 
+    @property
+    def reward_range(self):
+        return self._reward_range
+
     @action_space.setter
     def action_space(self, value):
         self._action_space = value
@@ -23,10 +27,15 @@ class GymEnvironmentWrapper(Environment):
     def observation_space(self, value):
         self._observation_space = value
 
+    @reward_range.setter
+    def reward_range(self, value):
+        self._reward_range = value
+
     def __init__(self, environment_name):
         self._gym_environment: gym.Env = gym.make(environment_name)
         self._action_space = self._gym_environment.action_space
         self._observation_space = self._gym_environment.observation_space
+        self._reward_range = self._gym_environment.reward_range
 
     def step(self, action):
         return self._gym_environment.step(action)
