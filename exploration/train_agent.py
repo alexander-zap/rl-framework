6b734a56013118d138351e244afba1528310c588
Alexander Zap <alexander.zap@alexanderthamm.com>
2023-02-23 19:46:42 +0100

    Moved various utility functions to util.py

--- exploration/train_agent.py
+++ exploration/train_agent.py
@@ -1,53 +1,39 @@
-import gym
+from rl_framework.util import evaluate, upload_to_huggingface_hub
 from rl_framework.agent.stable_baselines import StableBaselinesAgent
 from rl_framework.environment.gym_environment import GymEnvironmentWrapper
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.vec_env import DummyVecEnv
 
 ENV_ID = "LunarLander-v2"
 MODEL_ARCHITECTURE = "PPO"
-parallel_environments = 4
+PARALLEL_ENVIRONMENTS = 32
 
-
-def evaluate(agent_to_evaluate, evaluation_environment):
-    model = agent_to_evaluate.model
-    mean_reward, std_reward = evaluate_policy(model, evaluation_environment, n_eval_episodes=10, deterministic=True)
-    print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
-
-
-def upload_to_huggingface_hub(agent_to_upload, evaluation_environment):
-    repo_id = f"zap-thamm/{MODEL_ARCHITECTURE}-{ENV_ID}"
-    commit_message = f"Upload of a new agent trained with {MODEL_ARCHITECTURE} on {ENV_ID}"
-
-    # Create a Stable-baselines3 vector environment (required for HuggingFace upload function)
-    vec_env = DummyVecEnv([lambda: evaluation_environment])
-
-    model = agent_to_upload.model
-
-    # package_to_hub(
-    #     model=model,
-    #     model_name=model_name,
-    #     model_architecture=MODEL_ARCHITECTURE,
-    #     env_id=ENV_ID,
-    #     eval_env=vec_env,
-    #     repo_id=repo_id,
-    #     commit_message=commit_message,
-    # )
-
+MODEL_NAME = f"{MODEL_ARCHITECTURE}-{ENV_ID}"
+REPO_ID = f"zap-thamm/{MODEL_NAME}"
+COMMIT_MESSAGE = f"Upload of a new agent trained with {MODEL_ARCHITECTURE} on {ENV_ID}"
 
 if __name__ == "__main__":
     # Create environment(s); multiple environments for parallel training
-    environments = [GymEnvironmentWrapper(ENV_ID) for _ in range(parallel_environments)]
+    environments = [GymEnvironmentWrapper(ENV_ID) for _ in range(PARALLEL_ENVIRONMENTS)]
 
     # Create agent
     agent = StableBaselinesAgent(environments)
+
+    # Train agent
     agent.train()
 
     # Optional: Save the model
-    agent.save(file_path=f"{MODEL_ARCHITECTURE}-{ENV_ID}")
+    agent.save(file_path=f"{MODEL_NAME}")
 
     # Evaluate the model
-    evaluate(agent_to_evaluate=agent, evaluation_environment=environments[0])
+    mean_reward, std_reward = evaluate(agent_to_evaluate=agent, evaluation_environment=environments[0])
+    print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
 
     # Upload the model
-    upload_to_huggingface_hub(agent, environments[0])
+    upload_to_huggingface_hub(
+        agent_to_upload=agent,
+        evaluation_environment=environments[0],
+        model_name=MODEL_NAME,
+        model_architecture=MODEL_ARCHITECTURE,
+        environment_name=ENV_ID,
+        repository_id=REPO_ID,
+        commit_message=COMMIT_MESSAGE
+    )
---
+++ src/rl_framework/util/util.py
@@ -0,0 +1,59 @@
+from huggingface_sb3 import package_to_hub
+from rl_framework.agent import Agent
+from rl_framework.environment import Environment
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import DummyVecEnv
+from typing import Text, Tuple
+
+
+def evaluate(
+    agent_to_evaluate: Agent,
+    evaluation_environment: Environment,
+    n_evaluation_episodes: int = 10,
+    deterministic_action: bool = True
+) -> Tuple[float, float]:
+    """
+    Evaluate the performance (average reward and standard deviation of the reward) by running the policy of a trained
+    agent on an environment.
+
+    Args:
+        agent_to_evaluate (Agent): Agent whose policy should be evaluated.
+        evaluation_environment (Environment): Environment, on which the agent's policy should be evaluated
+        n_evaluation_episodes (int): Number of episode to evaluate the agent
+        deterministic_action (bool): Whether to use deterministic or stochastic actions
+
+    Returns:
+        Tuple of:
+            average reward (float)
+            standard deviation of reward (float)
+    """
+    mean_reward, std_reward = evaluate_policy(agent_to_evaluate.model,
+                                              evaluation_environment,
+                                              n_eval_episodes=n_evaluation_episodes,
+                                              deterministic=deterministic_action)
+    return mean_reward, std_reward
+
+
+def upload_to_huggingface_hub(
+    agent_to_upload: Agent,
+    evaluation_environment: Environment,
+    model_name: Text,
+    model_architecture: Text,
+    environment_name: Text,
+    repository_id: Text,
+    commit_message: Text
+):
+    # Create a Stable-baselines3 vector environment (required for HuggingFace upload function)
+    vec_env = DummyVecEnv([lambda: evaluation_environment])
+
+    model = agent_to_upload.model
+
+    package_to_hub(
+        model=model,
+        model_name=model_name,
+        model_architecture=model_architecture,
+        env_id=environment_name,
+        eval_env=vec_env,
+        repo_id=repository_id,
+        commit_message=commit_message,
+    )
