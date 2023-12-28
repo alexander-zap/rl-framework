20a648074c823fe1f233a0d518f5e686146b1bc8
alexander-zap <65029346+alexander-zap@users.noreply.github.com>
2023-12-28 02:42:27 +0100

    Updated Connector interface to support logging of individual values

--- src/rl_framework/util/saving_and_loading/connector/base_connector.py
+++ src/rl_framework/util/saving_and_loading/connector/base_connector.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List, SupportsFloat, Text, Tuple
 
 
 @dataclass
@@ -17,9 +18,37 @@ class DownloadConfig(ABC):
 
 
 class Connector(ABC):
-    @abstractmethod
     def __init__(self):
-        raise NotImplementedError
+        """Initialize connector for uploading or downloading agents from the HuggingFace Hub.
+
+        All attributes which are relevant for uploading or downloading are set in the config object.
+        This is a conscious design decision to enable generic exchange of connectors and prevent the requirements
+        of parameter passing for upload/download method calls.
+
+        For repeated use of the same connector instance, change the attributes of the config object which is passed
+        to the upload/download method.
+
+        self attributes:
+            logging_history: Dictionary mapping each logged value name to a list of logged values, e.g.:
+            {
+                "Episode reward": [(50.6, 10), (90.5, 20), (150.3, 30), (200.0, 40)],
+                "Epsilon": [(1.0, 10), (0.74, 20), (0.46, 30), (0.15, 10)]
+            }
+            Elements of each list are tuples of timestep-value-points.
+        """
+        self.logging_history: Dict[Text, List[Tuple]] = defaultdict(list)
+
+    def log_value(self, timestep: int, value_scalar: SupportsFloat, value_name: Text) -> None:
+        """
+        Log scalar value to create a sequence of values over time steps.
+        Can be used afterward for visualization (e.g., plotting of value over time).
+
+        Args:
+            timestep: Time step which the scalar value corresponds to (x-value)
+            value_scalar: Scalar value which should be logged (y-value)
+            value_name: Name of scalar value (e.g., "avg. sum of reward")
+        """
+        self.logging_history[value_name].append((timestep, value_scalar))
 
     @abstractmethod
     def upload(self, config: UploadConfig, agent, evaluation_environment, *args, **kwargs) -> None:
