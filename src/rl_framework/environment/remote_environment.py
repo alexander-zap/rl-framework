ab6180d532cc40ddd9e838cbc0305e1794cbe439
alexander-zap <65029346+alexander-zap@users.noreply.github.com>
2023-12-28 19:43:48 +0100

    Redesign of Remote environment implementation
    - Split of RemoteEnvironment-communication-wrapper (remote_environment.py on framework/client side) and RemoteEnvironment application logic (remote_environment_management.py on server side)
    
    - Exchange of information between remote environment application and wrapper (bounds, dtype, space); used for setting wrapper attributes; various utility functions for extracting information and transformation from/to dm_env spec to/from Gym space
    
    - Exploration script for running a remote environment application in a separate process (once started: take port from console output and start a RemoteEnvironment from a train_*_agent.py script)

--- src/rl_framework/environment/remote_environment.py
+++ src/rl_framework/environment/remote_environment.py
@@ -1,269 +1,159 @@
 import logging
-from concurrent import futures
-from typing import Optional, SupportsFloat, Tuple
+from typing import Optional, SupportsFloat, Text, Tuple
 
-import grpc
-from dm_env import StepType
+import numpy as np
+from dm_env import StepType, specs
 from dm_env_rpc.v1 import connection as dm_env_rpc_connection
-from dm_env_rpc.v1 import (
-    dm_env_adaptor,
-    dm_env_rpc_pb2,
-    dm_env_rpc_pb2_grpc,
-    spec_manager,
-    tensor_spec_utils,
-)
-from google.rpc import code_pb2, status_pb2
-from gymnasium.spaces import Space
-from numpy.dtypes import Float32DType, Int64DType, UInt8DType
+from dm_env_rpc.v1 import dm_env_adaptor, dm_env_rpc_pb2
+from gymnasium.spaces import Box, Discrete, Space
 
 from rl_framework.environment import Environment
 
 
-def remote_environment(cls: type):
+class RemoteEnvironment(Environment):
     """
-    Decorator implementation of remote environments, with which every environment can be transformed to a remote one.
-    Implements central environment-side management for the open communication interface between the Environment class
-        and remotely running games.
-
-    Args:
-        cls: Environment class which should be extended with remote environment functionality.
-
-    Returns:
-        Decorated environment class.
-
+    Wrapper implementation of remote environments,
+    Implements central client-side management for the open communication interface between this wrapper Environment
+    class and remotely running environments.
     """
 
-    class RemoteEnvironment(cls, Environment):
-        def __init__(self, *args, **kwargs):
-            """
-            Initialize the environment.
-            """
+    @property
+    def action_space(self):
+        return self._action_space
 
-            def start_server(local_environment: Environment):
-                """Starts the Catch gRPC server."""
-                server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
-                servicer = RemoteEnvironmentService(environment=local_environment)
-                dm_env_rpc_pb2_grpc.add_EnvironmentServicer_to_server(servicer, server)
+    @property
+    def observation_space(self):
+        return self._observation_space
 
-                port = server.add_secure_port("localhost:0", grpc.local_server_credentials())
-                server.start()
-                return server, port
+    @property
+    def reward_range(self):
+        return self._reward_range
 
-            # Instantiate environment locally
-            environment = cls(*args, **kwargs)
+    @property
+    def render_mode(self):
+        return self._render_mode
 
-            # Set local environment attributes for remote wrapper
-            self._action_space = environment.action_space
-            self._observation_space = environment.observation_space
-            self._reward_range = environment.reward_range
-            # TODO: Render remote environments. Currently not possible.
-            self._render_mode = None
+    @action_space.setter
+    def action_space(self, value):
+        self._action_space = value
 
-            # Pass locally instantiated environment to gRPC server.
-            self.server, self.port = start_server(local_environment=environment)
+    @observation_space.setter
+    def observation_space(self, value):
+        self._observation_space = value
 
-            self.connection = dm_env_rpc_connection.create_secure_channel_and_connect(f"localhost:{self.port}")
-            self.remote_environment, self.world_name = dm_env_adaptor.create_and_join_world(
-                self.connection, create_world_settings={}, join_world_settings={}
-            )
+    @reward_range.setter
+    def reward_range(self, value):
+        self._reward_range = value
 
-        def step(self, action, *args, **kwargs) -> Tuple[object, SupportsFloat, bool, bool, dict]:
-            """
-            Run one timestep of the environment's dynamics.
-            Accepts an action and returns a tuple (observation, reward, done, info).
-            """
-            timestep = self.remote_environment.step({"action": action})
+    @render_mode.setter
+    def render_mode(self, value):
+        self._render_mode = value
 
-            observation = timestep.observation["observation"]
-            reward = timestep.reward
-            discount_factor = timestep.discount
-            step_type = timestep.step_type
-
-            terminated = False
-            truncated = False
-
-            if step_type is StepType.LAST:
-                # NOTE: Assumption that discount_factor is 0.0 only for termination steps
-                #   See https://github.com/google-deepmind/dm_env/blob/master/dm_env/_environment.py#L228
-                if discount_factor == 0.0:
-                    terminated = True
-                else:
-                    truncated = True
-
-            return observation, reward, terminated, truncated, {}
-
-        def reset(self, seed: Optional[int] = None, *args, **kwargs):
-            """
-            Resets the environment to an initial state.
-            Returns the initial observation.
-            """
-            timestep = self.remote_environment.reset()
-
-            observation = timestep.observation["observation"]
-
-            return observation, {}
-
-        def render(self):
-            """
-            Renders the environment.
-            """
-            # TODO: Render remote environments. Currently not possible.
-
-        # FIXME: Does not trigger in main-file, investigate.
-        def __del__(self):
-            self.remote_environment.__del__()
-            self.connection.send(dm_env_rpc_pb2.DestroyWorldRequest(world_name=self.world_name))
-            super(self.connection).close()
-            self.connection.close()
-            self.server.stop(None)
-
-    return RemoteEnvironment
-
-
-class RemoteEnvironmentService(dm_env_rpc_pb2_grpc.EnvironmentServicer):
-    """Runs the environment as a gRPC EnvironmentServicer."""
-
-    def __init__(self, environment: Environment):
-        self.environment = environment
-
-        def space_to_dtype(space: Space) -> Tuple:
-            """
-
-            Args:
-                space: Gymnasium Space object for definition of observation spaces
-
-            Returns:
-                TensorSpec.dtype
-
-            """
-            if isinstance(space.dtype, Int64DType):
-                dtype = dm_env_rpc_pb2.INT64
-            elif isinstance(space.dtype, Float32DType):
-                dtype = dm_env_rpc_pb2.FLOAT
-            elif isinstance(space.dtype, UInt8DType):
-                dtype = dm_env_rpc_pb2.UINT8
-            else:
-                logging.error(
-                    f"Unexpected dtype {space.dtype} of space {space}, cannot convert to TensorSpec-dtype."
-                    f"Support for this dtype can be added at the location of the raised ValueError."
-                )
-                raise ValueError
-
-            return dtype
-
-        self.observation_spec = {
-            1: dm_env_rpc_pb2.TensorSpec(
-                name="observation",
-                shape=self.environment.observation_space.shape,
-                dtype=space_to_dtype(self.environment.observation_space),
-            ),
-            2: dm_env_rpc_pb2.TensorSpec(name="reward", dtype=dm_env_rpc_pb2.FLOAT),
-        }
-        tensor_spec_utils.set_bounds(
-            self.observation_spec[2],
-            minimum=self.environment.reward_range[0],
-            maximum=self.environment.reward_range[1],
-        )
-
-        self.action_spec = {
-            1: dm_env_rpc_pb2.TensorSpec(
-                name="action",
-                shape=self.environment.action_space.shape,
-                dtype=space_to_dtype(self.environment.action_space),
-            )
-        }
-
-    def Process(self, request_iterator, context):
-        """Processes incoming EnvironmentRequests.
-
-        For each EnvironmentRequest the internal message is extracted and handled.
-        The response for that message is then placed in a EnvironmentResponse which
-        is returned to the client.
-
-        An error status will be returned if an unknown message type is received or
-        if the message is invalid for the current world state.
-
-
-        Args:
-          request_iterator: Message iterator provided by gRPC.
-          context: Context provided by gRPC.
-
-        Yields:
-          EnvironmentResponse: Response for each incoming EnvironmentRequest.
+    def __init__(self, url: Text, port: int, *args, **kwargs):
+        """
+        Initialize the remote environment connection.
         """
 
-        action_manager = spec_manager.SpecManager(self.action_spec)
-        observation_manager = spec_manager.SpecManager(self.observation_spec)
-        environment_should_be_reset = False
+        def convert_to_space(spec: specs.Array) -> Space:
+            if isinstance(spec, specs.DiscreteArray):
+                return Discrete(n=spec.num_values)
+            # UInt-check required for detecting pixel-based RGB observations (these should be embedded in the Box space)
+            if (
+                isinstance(spec, specs.BoundedArray)
+                and np.issubdtype(spec.dtype, np.integer)
+                and not isinstance(spec.dtype, np.dtypes.UInt8DType)
+            ):
+                return Discrete(n=spec.maximum - spec.minimum + 1, start=spec.minimum)
+            else:
+                if isinstance(spec, specs.BoundedArray):
+                    _min = spec.minimum
+                    _max = spec.maximum
 
-        for request in request_iterator:
-            environment_response = dm_env_rpc_pb2.EnvironmentResponse()
-
-            try:
-                message_type = request.WhichOneof("payload")
-                internal_request = getattr(request, message_type)
-
-                if message_type == "create_world":
-                    environment_should_be_reset = True
-                    response = dm_env_rpc_pb2.CreateWorldResponse(world_name="world")
-
-                elif message_type == "join_world":
-                    environment_should_be_reset = True
-                    response = dm_env_rpc_pb2.JoinWorldResponse()
-                    for uid, action_space in self.action_spec.items():
-                        response.specs.actions[uid].CopyFrom(action_space)
-                    for uid, observation_space in self.observation_spec.items():
-                        response.specs.observations[uid].CopyFrom(observation_space)
-
-                elif message_type == "step":
-                    if environment_should_be_reset:
-                        observation, info = self.environment.reset()
-                        reward = 0.0
-                        terminated = False
-                        truncated = False
-                        environment_should_be_reset = False
+                    if np.isscalar(_min) and np.isscalar(_max):
+                        # same min and max for every element
+                        return Box(low=_min, high=_max, shape=spec.shape, dtype=spec.dtype)
                     else:
-                        unpacked_actions = action_manager.unpack(internal_request.actions)
-                        action = unpacked_actions.get("action")
-                        observation, reward, terminated, truncated, info = self.environment.step(action)
-
-                    response = dm_env_rpc_pb2.StepResponse()
-                    packed_observations = observation_manager.pack({"observation": observation, "reward": reward})
-
-                    for requested_observation in internal_request.requested_observations:
-                        response.observations[requested_observation].CopyFrom(
-                            packed_observations[requested_observation]
+                        # different min and max for every element
+                        return Box(
+                            low=_min + np.zeros(spec.shape),
+                            high=_max + np.zeros(spec.shape),
+                            shape=spec.shape,
+                            dtype=spec.dtype,
                         )
-                    if terminated or truncated:
-                        response.state = dm_env_rpc_pb2.EnvironmentStateType.TERMINATED
-                        environment_should_be_reset = True
-                    else:
-                        response.state = dm_env_rpc_pb2.EnvironmentStateType.RUNNING
-
-                elif message_type == "reset":
-                    environment_should_be_reset = True
-                    response = dm_env_rpc_pb2.ResetResponse()
-                    for uid, action_space in self.action_spec.items():
-                        response.specs.actions[uid].CopyFrom(action_space)
-                    for uid, observation_space in self.observation_spec.items():
-                        response.specs.observations[uid].CopyFrom(observation_space)
-
-                elif message_type == "reset_world":
-                    environment_should_be_reset = True
-                    response = dm_env_rpc_pb2.ResetWorldResponse()
-
-                elif message_type == "leave_world":
-                    response = dm_env_rpc_pb2.LeaveWorldResponse()
-
-                elif message_type == "destroy_world":
-                    response = dm_env_rpc_pb2.DestroyWorldResponse()
-
+                elif isinstance(spec, specs.Array):
+                    return Box(-np.inf, np.inf, shape=spec.shape)
                 else:
-                    raise RuntimeError("Unhandled message: {}".format(message_type))
+                    logging.error(
+                        f"Unable to transform dm_env.spec {type(spec)} to Gym space."
+                        f"Support for this dm_env.spec type can be added at the location of the raised ValueError."
+                    )
+                    raise ValueError
 
-                getattr(environment_response, message_type).CopyFrom(response)
+        self.connection = dm_env_rpc_connection.create_secure_channel_and_connect(f"{url}:{port}")
+        self.remote_environment, self.world_name = dm_env_adaptor.create_and_join_world(
+            self.connection, create_world_settings={}, join_world_settings={}
+        )
 
-            except Exception as e:  # pylint: disable=broad-except
-                environment_response.error.CopyFrom(status_pb2.Status(code=code_pb2.INTERNAL, message=str(e)))
+        action_spec = self.remote_environment.action_spec()["action"]
+        observation_spec = self.remote_environment.observation_spec()["observation"]
+        reward_spec = self.remote_environment.reward_spec()
 
-            yield environment_response
+        # Set local environment attributes for remote wrapper
+        self._action_space = convert_to_space(action_spec)
+        self._observation_space = convert_to_space(observation_spec)
+        self._reward_range = (
+            (reward_spec.minimum, reward_spec.maximum)
+            if isinstance(reward_spec, specs.BoundedArray)
+            else (-float("inf"), float("inf"))
+        )
+
+        # TODO: Render remote environments. Currently not possible.
+        self._render_mode = None
+
+    def step(self, action, *args, **kwargs) -> Tuple[object, SupportsFloat, bool, bool, dict]:
+        """
+        Run one timestep of the environment's dynamics.
+        Accepts an action and returns a tuple (observation, reward, done, info).
+        """
+        timestep = self.remote_environment.step({"action": action})
+
+        observation = timestep.observation["observation"]
+        reward = timestep.reward
+        discount_factor = timestep.discount
+        step_type = timestep.step_type
+
+        terminated = False
+        truncated = False
+
+        if step_type is StepType.LAST:
+            # NOTE: Assumption that discount_factor is 0.0 only for termination steps
+            #   See https://github.com/google-deepmind/dm_env/blob/master/dm_env/_environment.py#L228
+            if discount_factor == 0.0:
+                terminated = True
+            else:
+                truncated = True
+
+        return observation, reward, terminated, truncated, {}
+
+    def reset(self, seed: Optional[int] = None, *args, **kwargs):
+        """
+        Resets the environment to an initial state.
+        Returns the initial observation.
+        """
+        timestep = self.remote_environment.reset()
+
+        observation = timestep.observation["observation"]
+
+        return observation, {}
+
+    def render(self):
+        """
+        Renders the environment.
+        """
+        # TODO: Render remote environments. Currently not possible.
+
+    # FIXME: Does not trigger in main-file, investigate.
+    def __del__(self):
+        self.connection.send(dm_env_rpc_pb2.DestroyWorldRequest(world_name=self.world_name))
+        self.connection.close()
+        self.remote_environment.__del__()
---
+++ src/rl_framework/util/remote_environment_management.py
@@ -0,0 +1,225 @@
+import logging
+from concurrent import futures
+from typing import Text, Tuple
+
+import grpc
+from dm_env_rpc.v1 import (
+    dm_env_rpc_pb2,
+    dm_env_rpc_pb2_grpc,
+    spec_manager,
+    tensor_spec_utils,
+)
+from google.rpc import code_pb2, status_pb2
+from gymnasium.spaces import Box, Discrete, Space
+from numpy.dtypes import Float32DType, Int64DType, UInt8DType
+
+from rl_framework.environment import Environment
+
+
+def start_as_remote_environment(local_environment: Environment, url: Text) -> Tuple[grpc.Server, int]:
+    """
+    Method with which every environment can be transformed to a remote one.
+    Use the Remote Environment class
+
+    Starts the Catch gRPC server and passes the locally instantiated environment.
+
+    Args:
+        local_environment: Environment which should be ran remotely on a server.
+        url: URL to the machine where the remote environment should be running on.
+
+    Returns:
+        server: Reference to the gRPC server (for later closing)
+        port: Port (on the remote machine URL) for communication with the remote environment.
+
+    """
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    servicer = RemoteEnvironmentService(environment=local_environment)
+    dm_env_rpc_pb2_grpc.add_EnvironmentServicer_to_server(servicer, server)
+
+    port = server.add_secure_port(f"{url}:0", grpc.local_server_credentials())
+    server.start()
+
+    logging.info(f"Remote environment running on {url}:{port}")
+
+    return server, port
+
+
+class RemoteEnvironmentService(dm_env_rpc_pb2_grpc.EnvironmentServicer):
+    """Runs the environment as a gRPC EnvironmentServicer."""
+
+    def __init__(self, environment: Environment):
+        self.environment = environment
+
+        def space_to_dtype(space: Space) -> dm_env_rpc_pb2.DataType:
+            """Extract the dm_env_rpc_pb2 data type from the Gym Space.
+
+            Args:
+                space: Gymnasium Space object for definition of observation spaces
+
+            Returns:
+                dtype of the TensorSpec
+            """
+            if isinstance(space.dtype, Int64DType):
+                dtype = dm_env_rpc_pb2.INT64
+            elif isinstance(space.dtype, Float32DType):
+                dtype = dm_env_rpc_pb2.FLOAT
+            elif isinstance(space.dtype, UInt8DType):
+                dtype = dm_env_rpc_pb2.UINT8
+            else:
+                logging.error(
+                    f"Unexpected dtype {space.dtype} of space {space}, cannot convert to TensorSpec-dtype."
+                    f"Support for this dtype can be added at the location of the raised ValueError."
+                )
+                raise ValueError
+
+            return dtype
+
+        def space_to_bounds(space: Space) -> Tuple:
+            """Extract the upper and lower bounds of the Gym space.
+
+            Args:
+                space: Gymnasium Space object for definition of observation spaces
+
+            Returns:
+                Tuple (lower and upper and lower bounds of Gym space in the shape of the Gym shape)
+            """
+            if isinstance(space, Discrete):
+                return space.start, space.start + space.n - 1
+            elif isinstance(space, Box):
+                return space.low, space.high
+            else:
+                logging.error(
+                    f"Unexpected space type {type(space)} of space {space}, cannot extract higher and lower bounds."
+                    f"Support for this space type can be added at the location of the raised ValueError."
+                )
+                raise ValueError
+
+        self.action_spec = {
+            1: dm_env_rpc_pb2.TensorSpec(
+                name="action",
+                shape=self.environment.action_space.shape,
+                dtype=space_to_dtype(self.environment.action_space),
+            )
+        }
+
+        self.observation_spec = {
+            1: dm_env_rpc_pb2.TensorSpec(
+                name="observation",
+                shape=self.environment.observation_space.shape,
+                dtype=space_to_dtype(self.environment.observation_space),
+            ),
+            2: dm_env_rpc_pb2.TensorSpec(name="reward", dtype=dm_env_rpc_pb2.FLOAT),
+        }
+
+        action_space_bounds = space_to_bounds(self.environment.action_space)
+        observation_space_bounds = space_to_bounds(self.environment.observation_space)
+
+        tensor_spec_utils.set_bounds(
+            self.action_spec[1], minimum=action_space_bounds[0], maximum=action_space_bounds[1]
+        )
+
+        tensor_spec_utils.set_bounds(
+            self.observation_spec[1], minimum=observation_space_bounds[0], maximum=observation_space_bounds[1]
+        )
+
+        tensor_spec_utils.set_bounds(
+            self.observation_spec[2],
+            minimum=self.environment.reward_range[0],
+            maximum=self.environment.reward_range[1],
+        )
+
+    def Process(self, request_iterator, context):
+        """Processes incoming EnvironmentRequests.
+
+        For each EnvironmentRequest the internal message is extracted and handled.
+        The response for that message is then placed in a EnvironmentResponse which
+        is returned to the client.
+
+        An error status will be returned if an unknown message type is received or
+        if the message is invalid for the current world state.
+
+
+        Args:
+          request_iterator: Message iterator provided by gRPC.
+          context: Context provided by gRPC.
+
+        Yields:
+          EnvironmentResponse: Response for each incoming EnvironmentRequest.
+        """
+
+        action_manager = spec_manager.SpecManager(self.action_spec)
+        observation_manager = spec_manager.SpecManager(self.observation_spec)
+        environment_should_be_reset = False
+
+        for request in request_iterator:
+            environment_response = dm_env_rpc_pb2.EnvironmentResponse()
+
+            try:
+                message_type = request.WhichOneof("payload")
+                internal_request = getattr(request, message_type)
+                logging.debug(f"Received message of type {message_type}.")
+
+                if message_type == "create_world":
+                    environment_should_be_reset = True
+                    response = dm_env_rpc_pb2.CreateWorldResponse(world_name="world")
+
+                elif message_type == "join_world":
+                    environment_should_be_reset = True
+                    response = dm_env_rpc_pb2.JoinWorldResponse()
+                    for uid, action_space in self.action_spec.items():
+                        response.specs.actions[uid].CopyFrom(action_space)
+                    for uid, observation_space in self.observation_spec.items():
+                        response.specs.observations[uid].CopyFrom(observation_space)
+
+                elif message_type == "step":
+                    if environment_should_be_reset:
+                        observation, info = self.environment.reset()
+                        reward = 0.0
+                        terminated = False
+                        truncated = False
+                        environment_should_be_reset = False
+                    else:
+                        unpacked_actions = action_manager.unpack(internal_request.actions)
+                        action = unpacked_actions.get("action")
+                        observation, reward, terminated, truncated, info = self.environment.step(action)
+
+                    response = dm_env_rpc_pb2.StepResponse()
+                    packed_observations = observation_manager.pack({"observation": observation, "reward": reward})
+
+                    for requested_observation in internal_request.requested_observations:
+                        response.observations[requested_observation].CopyFrom(
+                            packed_observations[requested_observation]
+                        )
+                    if terminated or truncated:
+                        response.state = dm_env_rpc_pb2.EnvironmentStateType.TERMINATED
+                        environment_should_be_reset = True
+                    else:
+                        response.state = dm_env_rpc_pb2.EnvironmentStateType.RUNNING
+
+                elif message_type == "reset":
+                    environment_should_be_reset = True
+                    response = dm_env_rpc_pb2.ResetResponse()
+                    for uid, action_space in self.action_spec.items():
+                        response.specs.actions[uid].CopyFrom(action_space)
+                    for uid, observation_space in self.observation_spec.items():
+                        response.specs.observations[uid].CopyFrom(observation_space)
+
+                elif message_type == "reset_world":
+                    environment_should_be_reset = True
+                    response = dm_env_rpc_pb2.ResetWorldResponse()
+
+                elif message_type == "leave_world":
+                    response = dm_env_rpc_pb2.LeaveWorldResponse()
+
+                elif message_type == "destroy_world":
+                    response = dm_env_rpc_pb2.DestroyWorldResponse()
+
+                else:
+                    raise RuntimeError("Unhandled message: {}".format(message_type))
+
+                getattr(environment_response, message_type).CopyFrom(response)
+
+            except Exception as e:  # pylint: disable=broad-except
+                environment_response.error.CopyFrom(status_pb2.Status(code=code_pb2.INTERNAL, message=str(e)))
+
+            yield environment_response
